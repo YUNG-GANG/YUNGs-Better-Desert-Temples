29fcba254f4307729a8288877dfb849e4816b611
Nick Young <yungnickyoung@gmail.com>
2022-02-23 03:25:15 -0500

    Add banner processing!

--- src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/init/BDTModProcessors.java
+++ src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/init/BDTModProcessors.java
@@ -14,6 +14,7 @@ public class BDTModProcessors {
     public static StructureProcessorType<SpongeProcessor> SPONGE_PROCESSOR = () -> SpongeProcessor.CODEC;
     public static StructureProcessorType<EndStoneBrickWallProcessor> END_STONE_BRICK_WALL_PROCESSOR = () -> EndStoneBrickWallProcessor.CODEC;
     public static StructureProcessorType<WhiteStainedGlassProcessor> WHITE_STAINED_GLASS_PROCESSOR = () -> WhiteStainedGlassProcessor.CODEC;
+    public static StructureProcessorType<LimeBannerProcessor> LIME_BANNER_PROCESSOR = () -> LimeBannerProcessor.CODEC;
 
     public static void init() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(BDTModProcessors::commonSetup);
@@ -26,6 +27,7 @@ public class BDTModProcessors {
             Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "sponge_processor"), SPONGE_PROCESSOR);
             Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "end_stone_brick_wall_processor"), END_STONE_BRICK_WALL_PROCESSOR);
             Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "white_stained_glass_processor"), WHITE_STAINED_GLASS_PROCESSOR);
+            Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "lime_banner_processor"), LIME_BANNER_PROCESSOR);
         });
     }
 }
