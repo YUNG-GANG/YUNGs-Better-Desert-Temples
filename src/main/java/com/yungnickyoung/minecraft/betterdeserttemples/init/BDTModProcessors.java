290b96ffe0f5c4511837d045451400380219c44f
Nick Young <yungnickyoung@gmail.com>
2022-02-23 02:15:00 -0500

    Finish adding rooms to top and mid levels.

--- src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/init/BDTModProcessors.java
+++ src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/init/BDTModProcessors.java
@@ -1,8 +1,7 @@
 package com.yungnickyoung.minecraft.betterdeserttemples.init;
 
 import com.yungnickyoung.minecraft.betterdeserttemples.BetterDesertTemples;
-import com.yungnickyoung.minecraft.betterdeserttemples.world.processor.DioriteProcessor;
-import com.yungnickyoung.minecraft.betterdeserttemples.world.processor.PolishedDioriteProcessor;
+import com.yungnickyoung.minecraft.betterdeserttemples.world.processor.*;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
@@ -12,6 +11,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class BDTModProcessors {
     public static StructureProcessorType<PolishedDioriteProcessor> POLISHED_DIORITE_PROCESSOR = () -> PolishedDioriteProcessor.CODEC;
     public static StructureProcessorType<DioriteProcessor> DIORITE_PROCESSOR = () -> DioriteProcessor.CODEC;
+    public static StructureProcessorType<SpongeProcessor> SPONGE_PROCESSOR = () -> SpongeProcessor.CODEC;
+    public static StructureProcessorType<EndStoneBrickWallProcessor> END_STONE_BRICK_WALL_PROCESSOR = () -> EndStoneBrickWallProcessor.CODEC;
+    public static StructureProcessorType<WhiteStainedGlassProcessor> WHITE_STAINED_GLASS_PROCESSOR = () -> WhiteStainedGlassProcessor.CODEC;
 
     public static void init() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(BDTModProcessors::commonSetup);
@@ -21,6 +23,9 @@ public class BDTModProcessors {
         event.enqueueWork(() -> {
             Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "polished_diorite_processor"), POLISHED_DIORITE_PROCESSOR);
             Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "diorite_processor"), DIORITE_PROCESSOR);
+            Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "sponge_processor"), SPONGE_PROCESSOR);
+            Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "end_stone_brick_wall_processor"), END_STONE_BRICK_WALL_PROCESSOR);
+            Registry.register(Registry.STRUCTURE_PROCESSOR, new ResourceLocation(BetterDesertTemples.MOD_ID, "white_stained_glass_processor"), WHITE_STAINED_GLASS_PROCESSOR);
         });
     }
 }
