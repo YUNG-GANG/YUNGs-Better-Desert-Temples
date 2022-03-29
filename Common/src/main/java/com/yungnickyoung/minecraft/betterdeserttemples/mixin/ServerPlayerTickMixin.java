b119dbe1fdaf6244ef639798c4bd2461d4a3c48b
Nick Young <yungnickyoung@gmail.com>
2022-03-29 00:03:13 -0400

    Play sound upon acquiring mining fatigue. Place missing filled hall pieces.

--- Common/src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/mixin/ServerPlayerTickMixin.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/mixin/ServerPlayerTickMixin.java
@@ -4,16 +4,21 @@ import com.mojang.authlib.GameProfile;
 import com.yungnickyoung.minecraft.betterdeserttemples.BetterDesertTemplesCommon;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
+import net.minecraft.network.protocol.game.ClientboundSoundPacket;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,6 +31,9 @@ public abstract class ServerPlayerTickMixin extends Player {
         super(level, blockPos, f, gameProfile);
     }
 
+    @Shadow
+    public ServerGamePacketListenerImpl connection;
+
     @Inject(method = "tick", at = @At("HEAD"))
     private void injectMethod(CallbackInfo info) {
         if (this.tickCount % 20 == 0) {
@@ -36,6 +44,9 @@ public abstract class ServerPlayerTickMixin extends Player {
                     && ((ServerLevel) this.level).structureFeatureManager().getStructureWithPieceAt(blockpos, templeKey).isValid();
             if (isInTemple && BetterDesertTemplesCommon.CONFIG.general.applyMiningFatigue) {
                 if (!this.hasEffect(MobEffects.DIG_SLOWDOWN) || this.getEffect(MobEffects.DIG_SLOWDOWN).getAmplifier() < 2 || this.getEffect(MobEffects.DIG_SLOWDOWN).getDuration() < 120) {
+                    if (!this.hasEffect(MobEffects.DIG_SLOWDOWN)) {
+                        this.connection.send(new ClientboundSoundPacket(SoundEvents.ELDER_GUARDIAN_CURSE, SoundSource.HOSTILE, this.getX(), this.getY(), this.getZ(), 1.0F, 1.0F));
+                    }
                     this.addEffect(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 600, 2), this);
                 }
             }
