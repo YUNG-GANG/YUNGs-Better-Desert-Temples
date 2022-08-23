68ade4b6b33a3032f315c37fa269ec4832dc2b96
Nick Young <yungnickyoung@gmail.com>
2022-08-23 02:40:33 -0400

    v2.1. Port to 1.19.2.

--- Forge/src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/module/ConfigModuleForge.java
+++ Forge/src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/module/ConfigModuleForge.java
@@ -6,7 +6,7 @@ import com.yungnickyoung.minecraft.betterdeserttemples.world.ArmorStandChances;
 import com.yungnickyoung.minecraft.betterdeserttemples.world.ItemFrameChances;
 import com.yungnickyoung.minecraft.yungsapi.io.JSON;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
@@ -30,7 +30,7 @@ public class ConfigModuleForge {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(ConfigModuleForge::onConfigChange);
     }
 
-    private static void onWorldLoad(WorldEvent.Load event) {
+    private static void onWorldLoad(LevelEvent.Load event) {
         bakeConfig();
     }
 
