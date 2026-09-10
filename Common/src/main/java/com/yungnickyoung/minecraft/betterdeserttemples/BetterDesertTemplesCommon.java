9f9bd94339b762f5de4b1d69ef07b2c29bc7e21f
williambl <william@williambl.com>
2026-09-10 14:01:51 +0100

    Switch to using new YUNG's API LocateReplacer API
    YUNG-GANG/YUNGs-Better-Desert-Temples#51

--- Common/src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/BetterDesertTemplesCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterdeserttemples/BetterDesertTemplesCommon.java
@@ -3,6 +3,11 @@ package com.yungnickyoung.minecraft.betterdeserttemples;
 import com.yungnickyoung.minecraft.betterdeserttemples.module.ConfigModule;
 import com.yungnickyoung.minecraft.betterdeserttemples.services.Services;
 import com.yungnickyoung.minecraft.yungsapi.api.YungAutoRegister;
+import com.yungnickyoung.minecraft.yungsapi.api.world.structure.locate.LocateReplacer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,5 +22,8 @@ public class BetterDesertTemplesCommon {
     public static void init() {
         YungAutoRegister.scanPackageForAnnotations("com.yungnickyoung.minecraft.betterdeserttemples.module");
         Services.MODULES.loadModules();
+        LocateReplacer.register(BuiltinStructures.DESERT_PYRAMID,
+                ResourceKey.create(Registries.STRUCTURE, Identifier.fromNamespaceAndPath(MOD_ID, "desert_temple")),
+                () -> CONFIG.general.disableVanillaPyramids);
     }
 }
